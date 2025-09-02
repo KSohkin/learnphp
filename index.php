5d6421205e14c07b92e7c0c17f262673c6168445
KSohkin <Kaupo.Sohkin@tptlive.ee>
2025-09-02 11:14:19 +0300

    Add types examples

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hello World!\n";
+echo ("Hello World!\n");
+
+$var = 1;
+
+echo $var;
+echo 'var is $var';
+echo "\nvar is $var\n";
+echo $var . "\n";
+$num = 1;
+$num = $num + 1;
+var_dump($num);
