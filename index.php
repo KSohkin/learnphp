30c436ea730f8b255890c3dac467eca2a849c8f8
KSohkin <Kaupo.Sohkin@tptlive.ee>
2025-12-16 00:44:05 +0200

    Add boolean math example

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
-
-
 $bool = true && false;
 $bool = true || false;
 $bool = !false;
-
-$bool = true && false;
+$bool = (true && false || false) && !(true || false && true);
+$bool = 10 > 5;
+$bool = 10 < 5;
+$bool = 10 > 10;
+$bool = 10 < 10;
+$bool = 10 == 10;
+$bool = 10 >= 10;
+$bool = 10 <= 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 !== '10';
+$bool = 10 != '10';
 var_dump($bool);
