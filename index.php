69f6fd8ad2bd96744980cc2465bdbcad8b98a695
KSohkin <Kaupo.Sohkin@tptlive.ee>
2025-12-16 00:53:52 +0200

    objects by reference example

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
 <?php
-class Box {
-    public $width;
-    public $heigth;
-    public $length;
-    
-    public function __construct($w=0, $h=0, $l=0)
-    {   
-        $this->width = $w;
-        $this->heigth = $h;
-        $this->length = $l;
-    }
-public function volume(){
-        return $this->width * $this->heigth * $this->length;
-    }
-}
-
-$box1 = new Box(10, 10, 10);
-
-
-
-var_dump($box1);
-var_dump($box1->volume());
-
-$box2 = new Box(20, 20, 20);
-
-
-var_dump($box2);
-var_dump($box2->volume());
-var_dump($box1);
+$box1 = new Box();
+$box1->width = 10;
+$box2 = clone $box1;
+$box2->width = 20;
+var_dump($box1->width);
+var_dump($box1, $box2);
