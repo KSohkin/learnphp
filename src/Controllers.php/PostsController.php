4c89047c66b187ad8abfbfdba6240a1086142dd6
KSohkin <Kaupo.Sohkin@tptlive.ee>
2025-12-16 10:46:52 +0200

    Add create post and list post pages

---
+++ src/Controllers.php/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use App\DB;
+use App\Models\Post;
+use App\Models\User;
+
+class PostsController
+{
+    public function index()
+    {
+       $posts = Post::all();
+       view('posts/index', compact('posts'));
+    }
+
+    public function create() {
+        view('posts/create');
+    }
+
+    public function store() {
+        $post = new Post();
+        $post->title = $_POST['title'];
+        $post->body = $_POST['body'];
+        $post->save();
+        header('Location: /posts');
+    }
+}
